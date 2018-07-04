b8ef7ffbdcefebd98cfbdd3361be9f706ea658d4
4000D <dkdkajej@gmail.com>
2018-07-04 16:15:57 +0900

    use correct term(upfront cost), change function names

--- concept1.pseudo.py
+++ concept1.pseudo.py
@@ -1,9 +1,9 @@
 def tx_execute_before(from, to, value, gas, gasPrice, data) :
-    # 1. check intrinsic gas + value
+    # 1. check `from` can pay upfront cost
     balance = getBalance(from)
     assert balance >= value + gas * gasPrice
 
-    # 2. subtract gas + value
+    # 2. subtract upfront cost(tx.value + tx.gas * tx.gasPrice)
     subtractBalance(from, balance - value + gas * gasPrice)
 
     # 3. execute EVM
@@ -18,17 +18,17 @@ def tx_execute_after(from, to, value, gas, gasPrice, data) :
 
     # 1. case where delegatee exist
     if delegatee:
-        # 1-1. check intrinsic gas if `to` has delegatee
+        # 1-1. if `to` has delegatee check upfront cost(only gas*gasPrice) 
         assert staminaContract.balanceOf(delegatee) >= gas * gasPrice
 
-        # 1-2. subtract gas from delegatee
+        # 1-2. subtract upfront cost(only gas*gasPrice) from delegatee
         staminaContract.subtractBalance(delegatee, gas * gasPrice)
 
-        # 1-3. subtract value
+        # 1-3. subtract upfront cost(only value)
         subtractBalance(from, value)
     # 2. case where delegatee does not exist
     else:
-        # 2-1. check intrinsic gas + value
+        # 2-1. check `from` can pay upfront cost
         balance = getBalance(to)
         assert balance >= value + gas * gasPrice
 
--- concept2.pseudo.py
+++ concept2.pseudo.py
@@ -1,9 +1,9 @@
 def tx_execute_before(from, to, value, gas, gasPrice, data) :
-    # 1. check intrinsic gas + value
+    # 1. check `from` can pay upfront cost
     balance = getBalance(from)
     assert balance >= value + gas * gasPrice
 
-    # 2. subtract gas + value
+    # 2. subtract upfront cost(tx.value + tx.gas * tx.gasPrice)
     subtractBalance(from, balance - value + gas * gasPrice)
 
     # 3. execute EVM
@@ -12,48 +12,35 @@ def tx_execute_before(from, to, value, gas, gasPrice, data) :
     # 4. collect refunded gas
     addBalance(from, remainedGas * gasPrice)
 
-# "delegatee" is optional in transaction
+# "delegatee" is optional field in transaction
 def tx_execute_after(from, to, value, gas, gasPrice, data, delegatee=none):
     # 1. check if delegatee passed or not
     if delegatee:
-        # 1-1. check if delegatee account registered in staminaContract, executeVM
-        if (staminaContract.isDelegatee(delegatee)):
+        # 1-1. check if (`to`, `delegatee`) tuple is registered in staminaContract
+        if staminaContract.registered(to, delegatee):
+            # 1-1-1. check if `delegatee` can pay upfront cost(only gas * gasPrice)
+            assert staminaContract.balanceOf(delegatee) >= gas * gasPrice# 1-1-1. check if delegatee can pay upfront cost(only gas * gasPrice)
+            
+            # 1-1-2. check if `from` can pay upfront cost(only value)
+            assert getBalance(from) >= value
 
-            # 1-1-1. check intrinsic gas if `delegatee` has delegatee
-            assert staminaContract.balanceOf(delegatee) >= gas * gasPrice
-
-            # 1-1-2. subtract gas from delegatee
+            # 1-1-3. subtract upfront cost(only gas * gasPrice) from `delegatee`
             staminaContract.subtractBalance(delegatee, gas * gasPrice)
+            
+            # 1-1-4. subtract upfront cost(only value) from `from`
+            subtractBalance(from, value)
 
-            # 1-1-3. execute EVM
+            # 1-1-5. execute EVM
             executeVM(from, to, value, gas, gasPrice, data)
 
-            # 1-1-4. collect refunded gas
-            addBalance(delegatee, remainedGas * gasPrice)
-
-            # 1-1-4. return
-            return
+            # 1-1-6. collect refunded gas
+            staminaContract.addBalance(delegatee, remainedGas * gasPrice)
 
         # 1-2 case where delegatee is not registered
         else:
             # "delegatee" account is in transaction but does not registered to staminaContract
-            # do nothing
-            assert()
-            return
+            raise # throw if (`to`, `delegatee`) tuple is not registered
 
-      # 2. normal transaction
+    # 2. transaction without delegatee. same logic as original version
     else:
-        # 2-1. check intrinsic gas + value
-        balance = getBalance(from)
-
-        # 2-2. check intrinsic gas if `delegatee` has delegatee
-        assert balance >= value + gas * gasPrice
-
-        # 2-3. subtract gas + value
-        subtractBalance(from, balance - value + gas * gasPrice)
-
-        # 2-4. execute EVM
-        executeVM(from, to, value, gas, gasPrice, data)
-
-        # 2-5. collect refunded gas
-        addBalance(from, remainedGas * gasPrice)
+        return tx_execute_before(from, to, value, gas, gasPrice, data)
